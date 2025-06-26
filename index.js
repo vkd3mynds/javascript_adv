84c314a4ceab83bc01ce648ae80afc87afdd9f72
Vikash Kumar <123033078+vkd3mynds@users.noreply.github.com>
2025-06-26 12:51:41 +0000

    "IMUTIBILITY"

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Functional Programming");
+// console.log("Functional Programming");
+
+// const myArr = [1,2,3,4];
+
+// const addToArr = (arr,data)=>{
+//     arr.push(data);
+
+//     return arr;
+// }
+// console.log(myArr)
+// console.log(addToArr(myArr,5))
+// console.log(myArr);
+
+
+// This is an excellent question that highlights a fundamental concept in JavaScript: pass-by-reference vs. pass-by-value for objects, and the difference between modifying an object in place (mutation) and creating a new object (immutability).
+
+let user = {
+    name: 'vikash',
+    age: 31
+}
+
+// function updateName(user, name){
+//     user.name = name
+// }
+
+function updateName(user,name){
+    let newObj = {...user,name:name};
+    return newObj;
+}
+console.log(user);
+updateName(user,"Vikrant");
+console.log(user);
+let updateObj = updateName(user,"Vikrant")
+console.log(updateObj);
